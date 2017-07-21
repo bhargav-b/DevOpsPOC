09d5e1b911e8ce0d9c15339f6ef5cb877da0a3a7
bhargav-b <bhargav.burugupalli@capgemini.com>
2017-07-21 16:03:24 +0530

    testing

--- spring-boot-web-jsp/src/main/java/com/cg/WelcomeController.java
+++ spring-boot-web-jsp/src/main/java/com/cg/WelcomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
-	// inject LLPP721via application.properties
+	// inject LLPP714via application.properties
 	@Value("${welcome.message:test}")
 	private String message = "Hello World";
 
